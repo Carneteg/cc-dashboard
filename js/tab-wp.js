1cb68eb411ce42280526ba45c01725926c934300
Carneteg <83163288+Carneteg@users.noreply.github.com>
2026-06-13 15:14:31 +0200

    Add js/tab-wp.js stub
    Created a stub for the Workforce Planning Classify tab module with a list of functions to be migrated.

---
+++ js/tab-wp.js
@@ -0,0 +1,8 @@
+// js/tab-wp.js  --  Workforce Planning + Classify tab module
+// STATUS: Stub -- functions still in index.html inline script.
+// Functions: destroyChart, initWP, loadCFOConfidence, setTimeGran, setTrendWindow,
+//   renderStaffingChart, loadAnalyticsTrend, renderTrendChart, renderTrendMonthlyCards,
+//   renderPoolGaps, renderPoolGapChart, renderScenarioChart, initClassifyTab,
+//   renderRoutingRulesGrid, renderProductMappingTable, hexToRgba, refreshClassificationData
+// To migrate: move here, import api.js, Export: { initWP, initClassifyTab }
+export const _TAB_WP_MIGRATED = false;
