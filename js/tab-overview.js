6b354966f9732fdfb9ca03cad67bbb867fc73846
Carneteg <83163288+Carneteg@users.noreply.github.com>
2026-06-13 15:18:23 +0200

    Add js/tab-overview.js stub
    This file serves as a stub for various tab-related functions and outlines migration tasks.

---
+++ js/tab-overview.js
@@ -0,0 +1,9 @@
+// js/tab-overview.js  --  Overview, AHT, Prognos, Setup tabs
+// STATUS: Stub. Functions in index.html.
+// loadOverview, fp, renderDailyTable, loadAHT, setView, filterAHT,
+// renderAHTGrid, updateEnrichStatus, runEnrich, initPrognos, runPrognos,
+// loadSetupTab, renderWorkdayTable, loadSetupStatus,
+// renderMigrationTable, renderMigrationChart, renderSensitivityTable, renderSensitivityChart
+// To migrate: import api.js, try/catch showError, fix runEnrich Supabase URL
+// Export: { loadOverview, loadAHT, initPrognos, loadSetupTab }
+export const _TAB_OVERVIEW_MIGRATED = false;
